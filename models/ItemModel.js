d118142295101206036277880fbd3bc9dbc262c6
Ashu491784 <ayeshamadushani01@icloud.com>
2025-07-31 11:33:29 +0530

    item

---
+++ models/ItemModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { create } = require("./userSchema");
+
+const itemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+
+});
